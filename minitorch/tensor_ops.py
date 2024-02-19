805550fee813a137d5e8fb4a8b3ea4ca86044edb
xuesu <5354339xy@gmail.com>
2024-02-19 17:13:59 +0100

    task3.2

--- minitorch/tensor_ops.py
+++ minitorch/tensor_ops.py
@@ -65,25 +65,25 @@ class MapProto(Protocol):
 class TensorOps:
     @staticmethod
     def map(fn: Callable[[float], float]) -> MapProto:
-        raise NotImplementedError()
+        pass  # type:ignore
 
     @staticmethod
     def cmap(fn: Callable[[float], float]) -> Callable[[Tensor, Tensor], Tensor]:
-        raise NotImplementedError()
+        pass  # type:ignore
 
     @staticmethod
     def zip(fn: Callable[[float, float], float]) -> Callable[[Tensor, Tensor], Tensor]:
-        raise NotImplementedError()
+        pass  # type:ignore
 
     @staticmethod
     def reduce(
         fn: Callable[[float, float], float], start: float = 0.0
     ) -> Callable[[Tensor, int], Tensor]:
-        raise NotImplementedError()
+        pass  # type:ignore
 
     @staticmethod
     def matrix_multiply(a: Tensor, b: Tensor) -> Tensor:
-        raise NotImplementedError("Not implemented in this assignment")
+        pass  # type:ignore
 
     cuda = False
 
